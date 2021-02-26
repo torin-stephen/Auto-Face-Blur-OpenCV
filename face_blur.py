88eceb67e12e48f1a13cb337e92623665e7b7c15
Adam Stephen (H0207) <adam.stephen@ukaea.uk>
2021-02-26 18:31:03 +0000

    Fix issue 6 and add command line blur confidence_threshold

--- face_blur.py
+++ face_blur.py
@@ -15,29 +15,25 @@ model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 
 class VideoTransformer():
 
-    def __init__(self, original_dir, blurred_dir):
+    def __init__(self, original_dir, blurred_dir, confidence_threshold, input_video):
         self.original_dir = original_dir
         self.blurred_dir = blurred_dir
+        self.confidence_threshold = confidence_threshold
+        self.input_video = input_video
         self.prepare_frames()
         self.n_frames = self.extract_stills()
         self.bar = ShadyBar("Blurring", max = self.n_frames)
         self.codec = cv2.VideoWriter_fourcc(*'MP4V')
-        self.blur_movie()
+        self.blur_movie(confidence_threshold)
         self.bar.finish()
         print("New video is in {} with {} frames".format(self.output_video, self.n_frames))
 
     def prepare_frames(self):
-        if len(sys.argv) > 1:
-            input_video = sys.argv[1]
-        else:
-            input_video = "input.mp4"
-
-        if not os.path.exists(input_video):
-            print("{} does not exist".format(input_video))
+        if not os.path.exists(self.input_video):
+            print("{} does not exist".format(self.input_video))
             sys.exit(2)
             
-        self.input_video = input_video
-        self.output_video = "blurred.{}".format(input_video)
+        self.output_video = "blurred.{}".format(self.input_video)
 
         if not os.path.exists("frames"):
             os.mkdir("frames")
@@ -60,7 +56,7 @@ class VideoTransformer():
             count += 1
         return count
 
-    def blur_movie(self):
+    def blur_movie(self, confidence_threshold):
         sample_image = cv2.imread(os.path.join(self.original_dir, "frame0.jpg"))
         height, width = sample_image.shape[:2]
         video = cv2.VideoWriter(self.output_video, self.codec, 15,  (width, height))#, True)
@@ -69,6 +65,7 @@ class VideoTransformer():
             filename = os.path.join(self.original_dir, "frame%d.jpg" % frame_id)
             image = cv2.imread(filename)
             h, w = image.shape[:2]
+            #print("Processing {} {}x{}".format(filename, h,w))
             kernel_width = (w // 7) | 1
             kernel_height = (h // 7) | 1
             blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
@@ -79,7 +76,7 @@ class VideoTransformer():
                 confidence = output[i, 2]
                 # get the confidence
                 # if confidence is above 40%, then blur the bounding box (face)
-                if confidence > 0.4:
+                if confidence > confidence_threshold:
                     # get the surrounding box cordinates and upscale them to original image
                     box = output[i, 3:7] * np.array([w, h, w, h])
                     start_x, start_y, end_x, end_y = box.astype(int)
@@ -88,17 +85,26 @@ class VideoTransformer():
                     face = cv2.GaussianBlur(face, (kernel_width, kernel_height), 0)
                     # put the blurred face into the original image
                     image[start_y: end_y, start_x: end_x] = face
-                    video.write(image)
-                    self.bar.next()
+            video.write(image)
+            self.bar.next()
         video.release()
 
     def __repr__(self):
-        return "A nice little Elf!"
+        return "A nicer little Elf!"
 
 def main():
     original_dir="./frames/original"
     blurred_dir="./frames/blurred"
-    vxform = VideoTransformer(original_dir, blurred_dir)
+    if len(sys.argv) > 1:
+        confidence_threshold = float(sys.argv[1])
+    else:
+        confidence_threshold = 0.4
+    if len(sys.argv) > 2:
+        input_video = sys.argv[2]
+    else:
+        input_video = "input.mp4"
+
+    vxform = VideoTransformer(original_dir, blurred_dir, confidence_threshold, input_video)
     print(vxform)
 
 if __name__ == '__main__':
