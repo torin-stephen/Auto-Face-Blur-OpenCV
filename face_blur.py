d27ed32d859e7a4ff1a4602032163d67e72900f4
Torin Stephen <tkmsmc@gmail.com>
2021-02-28 09:51:06 +0000

    fix issue 10

--- face_blur.py
+++ face_blur.py
@@ -1,4 +1,4 @@
-  
+
 #!/usr/bin/env python
 
 import cv2
@@ -11,8 +11,10 @@ from progress.bar import ShadyBar
 import glob
 
 prototxt_path = join(dirname(__file__), "deploy.prototxt")
-model_path = join(dirname(__file__), "res10_300x300_ssd_iter_140000_fp16.caffemodel")
+model_path = join(dirname(__file__),
+                  "res10_300x300_ssd_iter_140000_fp16.caffemodel")
 model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
 
 class VideoTransformer():
 
@@ -23,17 +25,18 @@ class VideoTransformer():
         self.input_video = input_video
         self.prepare_frames()
         self.n_frames = self.extract_stills()
-        self.bar = ShadyBar("Blurring", max = self.n_frames)
+        self.blur_bar = ShadyBar("Blurring", max=self.n_frames)
         self.codec = cv2.VideoWriter_fourcc(*'MP4V')
         self.blur_movie(confidence_threshold)
-        self.bar.finish()
-        print("New video is in {} with {} frames".format(self.output_video, self.n_frames))
+        self.blur_bar.finish()
+        print("New video is in {} with {} frames".format(
+            self.output_video, self.n_frames))
 
     def prepare_frames(self):
         if not os.path.exists(self.input_video):
             print("{} does not exist".format(self.input_video))
             sys.exit(2)
-            
+
         self.output_video = "blurred.{}".format(self.input_video)
 
         if not os.path.exists("frames"):
@@ -45,31 +48,41 @@ class VideoTransformer():
 
     def extract_stills(self):
         """Assume if there are any stills, we don't need to do this."""
-        existing = len(os.listdir(self.original_dir)) 
+        print("Extracting stills...")
+        existing = len(os.listdir(self.original_dir))
         if existing > 0:
-            print("Files exist in {} : skipping still extraction from {}".format(self.original_dir, self.input_video))
+            print("Files exist in {} : skipping still extraction from {}".format(
+                self.original_dir, self.input_video))
             return existing
         vidcap = cv2.VideoCapture(self.input_video)
-        success, image = vidcap.read()
+
+        success,image = vidcap.read()
         count = 0
+        success = True
         while success:
+            success,image = vidcap.read()
             cv2.imwrite(os.path.join(self.original_dir, "frame%d.jpg" % count), image)
             count += 1
         return count
 
+
     def blur_movie(self, confidence_threshold):
-        sample_image = cv2.imread(os.path.join(self.original_dir, "frame0.jpg"))
+        sample_image = cv2.imread(os.path.join(
+            self.original_dir, "frame0.jpg"))
         height, width = sample_image.shape[:2]
-        video = cv2.VideoWriter(self.output_video, self.codec, 15,  (width, height))#, True)
+        video = cv2.VideoWriter(
+            self.output_video, self.codec, 15,  (width, height))  # , True)
 
         for frame_id in range(0, self.n_frames):
-            filename = os.path.join(self.original_dir, "frame%d.jpg" % frame_id)
+            filename = os.path.join(
+                self.original_dir, "frame%d.jpg" % frame_id)
             image = cv2.imread(filename)
             h, w = image.shape[:2]
             #print("Processing {} {}x{}".format(filename, h,w))
             kernel_width = (w // 7) | 1
             kernel_height = (h // 7) | 1
-            blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+            blob = cv2.dnn.blobFromImage(
+                image, 1.0, (300, 300), (104.0, 177.0, 123.0))
             model.setInput(blob)
             output = np.squeeze(model.forward())
 
@@ -83,19 +96,21 @@ class VideoTransformer():
                     start_x, start_y, end_x, end_y = box.astype(int)
                     face = image[start_y: end_y, start_x: end_x]
                     # apply gaussian blur to this face
-                    face = cv2.GaussianBlur(face, (kernel_width, kernel_height), 0)
+                    face = cv2.GaussianBlur(
+                        face, (kernel_width, kernel_height), 0)
                     # put the blurred face into the original image
                     image[start_y: end_y, start_x: end_x] = face
             video.write(image)
-            self.bar.next()
+            self.blur_bar.next()
         video.release()
 
     def __repr__(self):
         return "A nicer little Elf!"
 
+
 def main():
-    original_dir="./frames/original"
-    blurred_dir="./frames/blurred"
+    original_dir = "./frames/original"
+    blurred_dir = "./frames/blurred"
     if len(sys.argv) > 1:
         confidence_threshold = float(sys.argv[1])
     else:
@@ -105,8 +120,10 @@ def main():
     else:
         input_video = "input.mp4"
 
-    vxform = VideoTransformer(original_dir, blurred_dir, confidence_threshold, input_video)
+    vxform = VideoTransformer(
+        original_dir, blurred_dir, confidence_threshold, input_video)
     print(vxform)
+
 
 if __name__ == '__main__':
     main()
